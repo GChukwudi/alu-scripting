1b4cf2415a7b742c11ce10d13814bb7a1942c83f
GChukwudi <g.chukwudi@alustudent.com>
2023-11-08 18:53:12 +0200

    Top Ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,7 +5,18 @@ import requests
 
 def top_ten(subreddit):
     """"top ten"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = 
-    response = requests.get(url)
-    data = response.json()['data']['children']
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    my_headers = {
+        "User-Agent": "myApp 0.1 (by /u/favour_DC)"
+        }
+
+    response = requests.get(URL, headers=my_headers)
+    raw_response = response.json()['data']['children']
+
+    if raw_response.status_code == 200:
+        json_response = raw_response.json()
+        post = json_response['data']['subscribers']
+        return post
+
+    else:
+        return 0
