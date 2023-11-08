90d4546eebfb241270bb87152041baec0d84d709
GChukwudi <g.chukwudi@alustudent.com>
2023-11-09 00:40:19 +0200

    count it!

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,18 +1,43 @@
 #!/usr/bin/python3
-""" recursive function that queries the Reddit API"""
+""""3-count.py"""
 import requests
-import sys
-after = None
-count_dic = []
 
 
-def count_words(subreddit, word_list):
-    """parses the title of all hot articles, and prints a sorted count of given
-    keywords (case-insensitive, delimited by spaces) """
-    global after
-    global count_dic
-    headers = {'User-Agent': 'myApp 0.1 (by /u/favour_DC)'}
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    parameters = {'after': after}
-    response = requests.get(url, headers=headers, allow_redirects=False,
-                            params=parameters)
+def count_words(subreddit, word_list, after="", words_count={}):
+    """"count words"""
+    if not words_count:
+        for word in word_list:
+            if word.lower() not in words_count:
+                words_count[word.lower()] = 0
+
+    if after is None:
+        wordict = sorted(words_count.items(), key=lambda x: (-x[1], x[0]))
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    my_headers = {
+        "User-Agent": "myApp 0.1 (by /u/favour_DC)"
+        }
+    params = {'after': after}
+    response = requests.get(URL, headers=my_headers, params=params)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        hot = response.json()['data']['children']
+        aft = response.json()['data']['after']
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
+
+            for word in words_count.keys():
+                words_count[word] += lower.count(word)
+
+    except Exception:
+        return None
+
+    count_words(subreddit, word_list, aft, words_count)
+    
