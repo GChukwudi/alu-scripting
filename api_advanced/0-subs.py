c58cf4291d0de22dd472d346db5b0eb428c0e13b
GChukwudi <g.chukwudi@alustudent.com>
2023-11-08 18:29:08 +0200

    file update

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""""Module"""
+""""Doc"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-        """Returns the number of subscribers for a given subreddit."""
-        subreddit = "programming"
-        URL = f"https://www.reddit.com/r/{subreddit}/about.json"
-
-        my_headers = {
-            "User-Agent": "Chrome 0.2 (by /u/favour_DC)"
+    """Returns the number of subscribers for a given subreddit."""
+    subreddit = "programming"
+    URL = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    my_headers = {
+        "User-Agent": "Chrome 0.2 (by /u/favour_DC)"
         }
-
-        raw_response = requests.get(URL, headers=my_headers, allow_redirects=False)
+    
+    raw_response = requests.get(URL, headers=my_headers, allow_redirects=False)
         
-        if raw_response.status_code == 200:
-            json_response = raw_response.json()
-            sub_count = json_response['data']['subscribers']
-            print(sub_count)
+    if raw_response.status_code == 200:
+        json_response = raw_response.json()
+        sub_count = json_response['data']['subscribers']
+        print(sub_count)
                 
-        else: 
-            print("Invalid Subreddit")
+    else: 
+        print("Invalid Subreddit")
 
 number_of_subscribers('programming')
