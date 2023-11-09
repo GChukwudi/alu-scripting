3822703fe04a270261e9a82d26db64fead43fc7c
GChukwudi <g.chukwudi@alustudent.com>
2023-11-09 16:07:23 +0200

    file updated

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -5,46 +5,38 @@ import requests
 
 def count_words(subreddit, word_list, after="", words_count={}):
     """"count words"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=100" \
-        .format(subreddit)
-    header = {'User-Agent': 'myApp 0.1 (by /u/favour_DC)'}
-    param = {'after': after}
-    response = requests.get(url, headers=header, params=param)
+    if not words_count:
+        for word in word_list:
+            if word.lower() not in words_count:
+                words_count[word.lower()] = 0
+
+    if after is None:
+        wordict = sorted(words_count.items(), key=lambda x: (-x[1], x[0]))
+        for word in wordict:
+            if word[1]:
+                print('{}: {}'.format(word[0], word[1]))
+        return None
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=100".format(subreddit)
+    my_headers = {
+        "User-Agent": "myApp 0.1 (by /u/favour_DC)"
+        }
+    params = {'after': after}
+    response = requests.get(URL, headers=my_headers, params=params)
 
     if response.status_code != 200:
-        return
+        return None
 
-    json_res = response.json()
-    after = json_res.get('data').get('after')
-    has_next = after is not None
-    hot_titles = []
-    words = [word.lower() for word in word_list]
+    try:
+        hot = response.json()['data']['children']
+        aft = response.json()['data']['after']
+        for post in hot:
+            title = post['data']['title']
+            lower = [word.lower() for word in title.split(' ')]
 
-    if len(words_count) == 0:
-        words_count = {word: 0 for word in words}
+            for word in words_count.keys():
+                words_count[word] += lower.count(word)
 
-    hot_articles = json_res.get('data').get('children')
-    [hot_titles.append(article.get('data').get('title'))
-     for article in hot_articles]
+    except Exception:
+        return None
 
-    # loop through all titles
-    for i in range(len(hot_titles)):
-        for title_word in hot_titles[i].lower().split():
-            for word in words:
-                if word.lower() == title_word:
-                    words_count[word] = words_count.get(word) + 1
-
-    if has_next:
-        return count_words(subreddit, word_list, after, words_count)
-    else:
-
-        words_count = dict(filter(lambda item: item[1] != 0,
-                                  words_count.items()))
-
-        words_count = sorted(words_count.items(),
-                             key=lambda item: item[1],
-                             reverse=True)
-
-        for i in range(len(words_count)):
-            print("{}: {}".format(words_count[i][0],
-                                  words_count[i][1]))
+    count_words(subreddit, word_list, aft, words_count)
